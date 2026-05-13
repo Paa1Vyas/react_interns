26d2649e1b77ab84cc9393d1bf50bc9bd6cbc57c
sahil2877 <sahilbelim5399@gmail.com>
2026-05-13 13:57:46 +0530

    resume addded

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,45 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import Hello from './Hello'
 import './App.css'
-import StateDemo from './StateDemo'
+// import StateDemo from './StateDemo'
+import Contact from "./components/Contact";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Education from "./components/Education";
+import ContactForm from "./components/ContactForm";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      {/* <h1>Hello MERN Stack</h1> 
-      <Hello name="Paavan" dept="Computer Science"></Hello> */}
-      <StateDemo></StateDemo>
-    </>
-  )
+    <div className="container">
+
+      <div className="card">
+
+        <h1>Sahil Belim</h1>
+
+        <p>Anand, Gujarat</p>
+
+        <Contact />
+
+        <hr />
+
+        <h2>Professional Summary</h2>
+
+        <p>
+          Pre-final year Computer Engineering student interested
+          in web development and real-world projects.
+        </p>
+
+        <Skills />
+
+        <Education />
+
+        <Projects />
+
+        <ContactForm />
+
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;

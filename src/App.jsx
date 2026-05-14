b243db845066dea6ae8b42bb9b6b63d0218d082c
Paavan Vyas <paavanvyas57@gmail.com>
2026-05-14 11:16:06 +0530

    Routing and Conditional Rendering Code

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,15 @@ import StateDemo from './StateDemo'
 import Navbar from './Navbar'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Home'
+import Login from './Login'
+import Profile from './Profile'
 
 import Form from './Form'
+import Dashboard from './Dashboard'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  let isLoggedin = false;
   return (
     <>
       {/* <h1>Hello MERN Stack</h1> 
@@ -24,8 +27,11 @@ function App() {
       <Navbar></Navbar>
       <BrowserRouter>
         <Routes>
-          <Route path="/home" element={<Home />}></Route>
+          <Route path="/" element={isLoggedin ? <Dashboard /> : <Home/>}></Route>
+          <Route path="/home" element={<Home  />}></Route>
           <Route path="/form" element={<Form />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
         </Routes>
       </BrowserRouter>
     </>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export default function Navbar() {
+    let isLoggedIn = true;
   return (
     <div>
         <nav className="navbar">
@@ -9,6 +10,7 @@ export default function Navbar() {
                 <li><a href="/">Home</a></li>
                 <li><a href="/about">About</a></li>
                 <li><a href="/contact">Contact</a></li>
+                <li>{isLoggedIn ? <a href="/profile">Profile</a> : <a href="/login">Login</a>}</li>
             </ul>
         </nav>
     </div>
